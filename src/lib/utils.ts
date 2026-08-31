1290b8786d7fe99e14304b2ce7eb9bbd328cc6fa
IahhMoments <isaiahchhay0@gmail.com>
2026-08-30 20:45:12 -0400

    added BASE_PATH

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,10 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
+export function assetPath(path: string) {
+  if (/^https?:\/\//.test(path)) return path;
+  return `${BASE_PATH}${path}`;
+}
